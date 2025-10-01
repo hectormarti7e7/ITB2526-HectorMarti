6c7fbf3221e733f1b82748f82d9e935c07a5f680
hector.marti.7e7 <hector.marti.7e7@itb.cat>
2025-10-01 09:21:15 +0200

    Comentaris posats

--- TA02/ComprovarEdat.py
+++ TA02/ComprovarEdat.py
@@ -1,5 +1,17 @@
+"""
+    Nom: Hector Marti
+    1/10/2025
+    ASIX1cB RA01 TA02
+    Programa que et comprova si ets major d'edat o no.
+"""
+#input de l'usuari on posa la seva edat en la variable
 edat = int(input("Quina edat tens? "))
+
+#Comprovació de la variable "edat", on mira si tens més o 18 anys, o tens menys de 18
 if edat >= 18 :
     print("Ets major d'edat!!!")
 else:
     print("No ets major d'edat!!!")
+
+#Fi del programa
+print("Fi del programa")
--- TA02/HelloWorld.py
+++ TA02/HelloWorld.py
@@ -1,1 +1,9 @@
+"""
+    Nom: Hector Marti
+    1/10/2025
+    ASIX1cB RA01 TA02
+    Hello World.
+"""
 print("Hello World")
+
+print("Fin del programa")
